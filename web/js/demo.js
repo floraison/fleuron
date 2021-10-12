89c1aa5b099f0c2bfe279d160d0aee02ce6e3557
John Mettraux <jmettraux@gmail.com>
2021-10-13 00:11:34 +0900

    Add favicon.ico

--- web/js/demo.js
+++ web/js/demo.js
@@ -1,3 +1,31 @@
 
 // demo.js
 
+var Demo = (function() {
+
+  "use strict";
+
+  var self = this;
+
+  //
+  // protected functions
+
+  var onFlowLoaded = function(res) {
+clog(res);
+  };
+
+  //
+  // public functions
+
+  this.loadFlow = function(name) {
+
+    H.request('GET', 'flo/' + name, onFlowLoaded);
+  };
+
+  //
+  // done.
+
+  return this;
+
+}).apply({}); // end Demo
+
