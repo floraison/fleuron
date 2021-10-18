3ebc2c6fe9d714f6439fde90b08b32201bfd0c47
John Mettraux <jmettraux@gmail.com>
2021-10-18 22:17:19 +0900

    Use grid for .fleuron

--- src/fleuron.js
+++ src/fleuron.js
@@ -22,37 +22,58 @@ var Fleuron = (function() {
     return e;
   };
 
+  var createFleuronDiv = function(elt, tree, name) {
+
+    var c = [ 'fleuron', `fleuron-${name}` ];
+    if (name !== tree[0]) c.push(`fleuron-${tree[0]}`)
+
+    var as = { class: c.join(' '), 'data-fleuron-line': tree[2] };
+    if (tree[3]) as['data-fleuron-file'] = tree[3];
+
+    var e = create(elt, 'div', as);
+    create(e, 'div', { class: 'fleuron-tree0' }, tree[0]);
+
+    return e;
+  };
+
   var rs = {};
 
   rs._children = function(elt, tree, parent) {
+
 //clog('renderChildren()', elt, tree, parent);
+    if ( ! Array.isArray(tree[1])) { render(ce, tree[1], parent); return elt; }
+
+    var ae = create(elt, 'div', { class: 'fleuron-attributes' });
     var ce = create(elt, 'div', { class: 'fleuron-children' });
-    if (Array.isArray(tree[1]))
-      tree[1].forEach(function(t) { render(ce, t, parent); });
-    else
-      render(ce, tree[1], parent);
+    var te = ae;
+
+    tree[1].forEach(function(t) {
+      if (t[0] !== '_att') te = ce;
+      render(te, t, parent);
+    });
+
+    if (ae.childElementCount < 1) elt.removeChild(ae);
+    if (ce.childElementCount < 1) elt.removeChild(ce);
+
     return elt;
   };
+
   rs._default = function(elt, tree, parent) {
 //clog('default', elt, tree);
-    //var p = { t0: tree[0] };
-    var e = create(
-      elt, 'div', {
-        class: `fleuron-default fleuron-${tree[0]}`,
-        'data-fleuron-line': tree[2],
-        'data-fleuron-file': tree[3] });
-    create(e, 'span', {}, tree[0]);
-    //var ce = create(e, 'div', { class: 'fleuron-children' });
-    //getRenderer(elt, '_children')(e, tree, p);
-    //return e;
+    var e = createFleuronDiv(elt, tree, '_default');
     return renderChildren(e, tree);
   };
+
   rs._leaf = function(elt, tree, parent) {
 //clog('_leaf', elt, tree);
     return create(elt,
       'span', { class: 'fleuron-_leaf' }, JSON.stringify(tree));
   };
 
+  //rs.sequence = function(elt, tree, parent) {
+  //  var e = createFleuronDiv(elt, tree, 'sequence');
+  //  return renderChildren(e, tree);
+  //};
   rs._sqs = function(elt, tree, parent) {
     var t = "'" + tree[1].replaceAll(/'/g, "\'") + "'";
     return create(elt, 'span', { class: 'fleuron-_sqs' }, t);
