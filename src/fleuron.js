a104281d64d85c95b0a3a8e259f4a5fae808f399
John Mettraux <jmettraux@gmail.com>
2023-03-27 11:32:17 +0900

    Don't add 1 to line number

--- src/fleuron.js
+++ src/fleuron.js
@@ -109,7 +109,7 @@ var Fleuron = (function() {
     create(be, 'div', { class: 'flrn-tree0' }, tree[0]);
 
     //h0e.title = 'nid: ' + tree[4];
-    h0e.title = `line ${1 + tree[2]} / nid ${tree[4]}`;
+    h0e.title = `line ${tree[2]} / nid ${tree[4]}`;
 
     e.setAttribute('data-flrn-nid', tree[4]);
 
